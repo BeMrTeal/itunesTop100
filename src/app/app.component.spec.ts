03ebfba9c872129f61342a71dd3d639ad3652cad
Bernardo Gomes <bernardo.med@gmail.com>
2022-05-13 23:36:17 +0100

    renaming

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe("AppComponent", () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'itunes-top100-angular'`, () => {
+  it(`should have as title 'itunes-top100'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual("itunes-top100-angular");
+    expect(app.title).toEqual("itunes-top100");
   });
 
   it("should render title in a h1 tag", () => {
@@ -25,7 +25,7 @@ describe("AppComponent", () => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector("h1").textContent).toContain(
-      "Welcome to itunes-top100-angular!"
+      "Welcome to itunes-top100!"
     );
   });
 });
